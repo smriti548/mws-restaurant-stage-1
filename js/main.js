9d8bd4fcb466605f3ea7e72a8295c894ed57f653
smriti548 <33401482+smriti548@users.noreply.github.com>
2018-07-01 21:41:00 +0530

    responsive index.html and design

--- js/main.js
+++ js/main.js
@@ -71,7 +71,7 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
 /**
  * Initialize leaflet map, called from HTML.
  */
-initMap = () => {
+/*initMap = () => {
   self.newMap = L.map('map', {
         center: [40.722216, -73.987501],
         zoom: 12,
@@ -87,8 +87,8 @@ initMap = () => {
   }).addTo(newMap);
 
   updateRestaurants();
-}
-/* window.initMap = () => {
+}*/
+ window.initMap = () => {
   let loc = {
     lat: 40.722216,
     lng: -73.987501
@@ -99,7 +99,12 @@ initMap = () => {
     scrollwheel: false
   });
   updateRestaurants();
-} */
+   let setTitle = () => {
+    const iFrameGoogleMaps = document.querySelector('#map iframe');
+    iFrameGoogleMaps.setAttribute('title', 'overview of restaurants');
+  }
+  self.map.addListener('tilesloaded', setTitle);
+} 
 
 /**
  * Update page and map for current restaurants.
@@ -157,35 +162,49 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
-
+li.className = 'restaurant-card';
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.setAttribute('alt', restaurant.alternative_text);
   li.append(image);
-
-  const name = document.createElement('h1');
+const divCardPrimary = document.createElement('div');
+  divCardPrimary.className = 'card-primary';
+  const name = document.createElement('h2');
+  name.className = 'card-title';
   name.innerHTML = restaurant.name;
-  li.append(name);
-
-  const neighborhood = document.createElement('p');
+  divCardPrimary.append(name);
+  const neighborhood = document.createElement('h3');
+  neighborhood.className = 'card-subtitle';
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  divCardPrimary.append(neighborhood);
+  li.append(divCardPrimary);
 
-  const address = document.createElement('p');
+ 
+  const divCardSecondary = document.createElement('div');
+  divCardSecondary.className = 'card-secondary';
+ 
+  const address = document.createElement('address');
+  address.className = 'card-secondary-content';
   address.innerHTML = restaurant.address;
-  li.append(address);
+  divCardSecondary.append(address);
+  li.append(divCardSecondary);
 
+  const divCardActions = document.createElement('div');
+  divCardActions.className = 'card-actions';
   const more = document.createElement('a');
+  more.className = 'card-actions-link';
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  divCardActions.append(more);
+  li.append(divCardActions);
 
   return li
 }
 
 /**
  * Add markers for current restaurants to the map.
- */
+ 
 addMarkersToMap = (restaurants = self.restaurants) => {
   restaurants.forEach(restaurant => {
     // Add marker to the map
@@ -197,8 +216,8 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 
-} 
-/* addMarkersToMap = (restaurants = self.restaurants) => {
+} */
+ addMarkersToMap = (restaurants = self.restaurants) => {
   restaurants.forEach(restaurant => {
     // Add marker to the map
     const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
@@ -207,5 +226,5 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     });
     self.markers.push(marker);
   });
-} */
+} 
 
